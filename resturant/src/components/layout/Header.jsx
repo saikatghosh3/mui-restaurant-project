2d0fb80368cce3d21fe1a5bc8781278fc53a84c9
Saikat Ghosh <saikat.blast20@gmail.com>
2025-01-04 01:58:30 +0600

    second

--- resturant/src/components/layout/Header.jsx
+++ resturant/src/components/layout/Header.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import { AppBar, Box, Toolbar, Typography } from '@mui/material';
+import FastfoodIcon from '@mui/icons-material/Fastfood';
+import { Link } from 'react-router-dom';
+import '../../style/HeaderStyles.css';
 
 const Header = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
-  )
-}
+    <>
+      <Box>
+        <AppBar component="nav" sx={{ bgcolor: 'black' }}>
+          <Toolbar>
+            <Typography color= {"goldenrod"} variant='h6' component={'div'} sx={{ flexGrow: 1 }}>
+              <FastfoodIcon/> My Restaurant </Typography>
 
-export default Header
+              <Box sx={{ display: { xs : "none", sm: "block"} }}>
+                <ul className='navigation-menu'>
+                  <li>
+                    <Link to={"/home"}>Home</Link>
+                  </li>
+                  <li>
+                    <Link to={"/menu"}>Menu</Link>
+                  </li>
+                  <li>
+                    <Link to={"/about"}>About</Link>
+                  </li>
+                  <li>
+                    <Link to={"/contact"}>Contact</Link>
+                  </li>
+                </ul>
+              </Box>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
+  );
+};
+
+export default Header;
