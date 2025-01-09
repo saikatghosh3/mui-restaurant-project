e9d4834adeba442f4a7926ee6f6cbc09bc11c6bf
Saikat Ghosh <saikat.blast20@gmail.com>
2025-01-10 03:06:07 +0600

    header

--- resturant/src/components/layout/Header.jsx
+++ resturant/src/components/layout/Header.jsx
@@ -1,35 +1,96 @@
-import { AppBar, Box, Toolbar, Typography } from '@mui/material';
+import { useState } from 'react';
+import { AppBar, Box, Divider, Drawer, IconButton, Toolbar, Typography } from '@mui/material';
 import FastfoodIcon from '@mui/icons-material/Fastfood';
 import { Link } from 'react-router-dom';
 import '../../style/HeaderStyles.css';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Header = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  // Handle menu click
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  // Menu drawer
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography color={"goldenrod"} variant="h6" component="div" sx={{ flexGrow: 1, my: 2 }}>
+        <FastfoodIcon /> My Restaurant
+      </Typography>
+      <Divider/>
+      <ul className="mobile-navigation">
+        <li>
+          <Link to={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link to={"/menu"}>Menu</Link>
+        </li>
+        <li>
+          <Link to={"/about"}>About</Link>
+        </li>
+        <li>
+          <Link to={"/contact"}>Contact</Link>
+        </li>
+      </ul>
+    </Box>
+  );
+
   return (
     <>
       <Box>
         <AppBar component="nav" sx={{ bgcolor: 'black' }}>
           <Toolbar>
-            <Typography color= {"goldenrod"} variant='h6' component={'div'} sx={{ flexGrow: 1 }}>
-              <FastfoodIcon/> My Restaurant </Typography>
-
-              <Box sx={{ display: { xs : "none", sm: "block"} }}>
-                <ul className='navigation-menu'>
-                  <li>
-                    <Link to={"/home"}>Home</Link>
-                  </li>
-                  <li>
-                    <Link to={"/menu"}>Menu</Link>
-                  </li>
-                  <li>
-                    <Link to={"/about"}>About</Link>
-                  </li>
-                  <li>
-                    <Link to={"/contact"}>Contact</Link>
-                  </li>
-                </ul>
-              </Box>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: "none" } }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography color={"goldenrod"} variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <FastfoodIcon /> My Restaurant
+            </Typography>
+            <Box sx={{ display: { xs: "none", sm: "block" } }}>
+              <ul className="navigation-menu">
+                <li>
+                  <Link to={"/"}>Home</Link>
+                </li>
+                <li>
+                  <Link to={"/menu"}>Menu</Link>
+                </li>
+                <li>
+                  <Link to={"/about"}>About</Link>
+                </li>
+                <li>
+                  <Link to={"/contact"}>Contact</Link>
+                </li>
+              </ul>
+            </Box>
           </Toolbar>
         </AppBar>
+        <Box component="nav">
+          <Drawer
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            sx={{ display: { xs: 'block', sm: 'none' },
+          "& .MuiDrawer-paper":{
+            boxSizing:"border-box", 
+            width: "240px",
+          },
+          }}
+          >
+            {drawer}
+          </Drawer>
+        </Box>
+        <Box sx={{ p: 1}}>
+        <Toolbar />
+        </Box>
+      
       </Box>
     </>
   );
